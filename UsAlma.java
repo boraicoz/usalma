1534a757638c913bddfa8aaf8e244232cc9ac252
boraicoz <94445388+boraicoz@users.noreply.github.com>
2022-12-10 17:41:28 +0300

    Add files via upload

---
+++ UsAlma.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class UsAlma {
+
+    public static int power(int taban,int us){
+        if(us==1)
+        {
+            return taban;
+        }
+
+
+        return taban*power(taban,us-1);
+
+
+
+
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        int taban,us;
+        Scanner in=new Scanner(System.in);
+        System.out.print("Taban:");
+        taban=in.nextInt();
+        System.out.print("Us:");
+        us=in.nextInt();
+        System.out.print(taban+"^"+us+":"+power(taban,us));
+    }
+}
